a243e2ba1b39c9f4b8e7a2aa740636de7ed4aadc
Alex Forsythe <awforsythe@gmail.com>
2021-09-21 21:17:12 -0500

    Experiment: print a big chunk of memory as floats

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <cinttypes>
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
 #include "ds_process.h"
 #include "ds_landmark.h"
 
@@ -22,7 +25,7 @@ struct ds_jumplist
 	}
 };
 
-int main()
+int main(int argc, char* argv[])
 {
 	ds_process process;
 	if (!process.open(L"DARK SOULS", L"DarkSoulsRemastered.exe"))
@@ -32,18 +35,38 @@ int main()
 	}
 
 	const ds_landmark landmark_x(0x3204E0, "48 8B 05 xx xx xx xx 48 39 48 68 0F 94 C0 C3");
-	const ds_jumplist x_to_player_pos{ {0x68, 0x18, 0x28, 0x50, 0x20, 0x120} };
+	const ds_jumplist x_to_player{ {0x68, 0x18, 0x28, 0x50, 0x20, 0x120 - 24} };
 	
 	const size_t offset_x = landmark_x.resolve_offset(process);
-	const size_t player_pos_offset = x_to_player_pos.resolve(offset_x, process);
+	const size_t player_offset = x_to_player.resolve(offset_x, process);
 
-	float player_pos[3];
+	const size_t chunk_size = 24;
+	const size_t num_chunks = 80;
+
+	system("cls");
+
+	float chunk[chunk_size];
 	while (true)
 	{
-		if (!process.read(player_pos_offset, reinterpret_cast<uint8_t*>(&player_pos), sizeof(player_pos)))
+		COORD coord;
+		coord.X = 0;
+		coord.Y = 0;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+		printf("          0       1       2       3       4       5       6       7       8       9      10      11      12      13      14      15      16      17      18      19      20      21      22      23\n");
+
+		size_t read_offset = player_offset;
+		for (size_t chunk_index = 0; chunk_index < num_chunks; chunk_index++)
 		{
-			break;
+			if (!process.read(read_offset, reinterpret_cast<uint8_t*>(chunk), sizeof(chunk)))
+			{
+				return 1;
+			}
+			printf("%02zu: %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f ", chunk_index, chunk[0], chunk[1], chunk[2], chunk[3], chunk[4], chunk[5], chunk[6], chunk[7], chunk[8], chunk[9], chunk[10], chunk[11]);
+			printf("%7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f\n", chunk[12], chunk[13], chunk[14], chunk[15], chunk[16], chunk[17], chunk[18], chunk[19], chunk[20], chunk[21], chunk[22], chunk[23]);
+			read_offset += chunk_size;
 		}
-		printf("%f, %f, %f\n", player_pos[0], player_pos[1], player_pos[2]);
+
+		Sleep(1);
 	}
 }
