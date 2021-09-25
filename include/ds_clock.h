0ea293a0428f07321ca1f5db8d1bca8624e32675
Alex Forsythe <awforsythe@gmail.com>
2021-09-25 01:06:02 -0500

    Count real time elapsed between frames in ds_clock
    This shows that elapsed time between frames (i.e. delta-time) is
    variable. Since a single frame in DS1R is fixed at 16 playtime ticks,
    this means that 1 tick != 1 ms; 1 tick == 1.0416666... ms.

--- include/ds_clock.h
+++ include/ds_clock.h
@@ -11,6 +11,8 @@ struct ds_clock
 	uint8_t* playtime_addr;
 	uint32_t playtime;
 	uint32_t frame_count;
+	uint64_t prev_frame_timestamp;
+	double real_frame_time;
 
 	ds_clock(ds_process& in_process, ds_addresses& in_addresses);
 	uint32_t read();
--- src/ds_clock.cpp
+++ src/ds_clock.cpp
@@ -1,14 +1,49 @@
 #include "ds_clock.h"
 
+#include <cinttypes>
+#include <cassert>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
 #include "ds_process.h"
 #include "ds_addresses.h"
+
+static uint64_t s_freq = 0;
+
+static uint64_t get_timestamp()
+{
+	LARGE_INTEGER val;
+	if (QueryPerformanceCounter(&val))
+	{
+		return val.QuadPart;
+	}
+	return 0;
+}
+
+static double get_elapsed(uint64_t from, uint64_t to)
+{
+	if (s_freq != 0)
+	{
+		return static_cast<double>(to - from) / s_freq;
+	}
+	return 0;
+}
 
 ds_clock::ds_clock(ds_process& in_process, ds_addresses& in_addresses)
 	: process(in_process)
 	, playtime_addr(in_addresses.playtime)
 	, playtime(0)
 	, frame_count(0)
+	, prev_frame_timestamp(0)
+	, real_frame_time(0.0)
 {
+	LARGE_INTEGER val;
+	if (QueryPerformanceFrequency(&val))
+	{
+		s_freq = val.QuadPart;
+	}
+	assert(s_freq != 0);
 }
 
 uint32_t ds_clock::read()
@@ -19,10 +54,21 @@ uint32_t ds_clock::read()
 	{
 		return 0;
 	}
+
 	const uint32_t delta = playtime - prev_playtime;
-	for (uint32_t i = 0; i < delta; i += 16)
+	if (delta > 0)
 	{
-		frame_count++;
+		for (uint32_t i = 0; i < delta; i += 16)
+		{
+			frame_count++;
+		}
+
+		const uint64_t timestamp = get_timestamp();
+		if (prev_frame_timestamp != 0)
+		{
+			real_frame_time = get_elapsed(prev_frame_timestamp, timestamp);
+		}
+		prev_frame_timestamp = timestamp;
 	}
 	return delta;
 }
