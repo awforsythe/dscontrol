33cf67d08351d2df3944cc48f2f553e8ec725b17
Alex Forsythe <awforsythe@gmail.com>
2021-09-25 01:06:30 -0500

    Move to Anor Londo map, kill player for consistent camera setup

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,8 @@ int main(int argc, char* argv[])
 	// Initialize our interfaces for reading/manipulating the game
 	ds_clock clock(process, addresses);
 	ds_player player(process, addresses);
-
+	
+	bool dropped_to_death = false;
 	while (true)
 	{
 		const uint32_t delta = clock.read();
@@ -67,39 +68,53 @@ int main(int argc, char* argv[])
 			const ds_pos pos = player.get_pos();
 
 			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, 0 });
-			printf("FRAME: %u             \n", clock.frame_count);
-			printf("POS X: %7.3f          \n", pos.x);
-			printf("POS Y: %7.3f          \n", pos.y);
-			printf("POS Z: %7.3f          \n", pos.z);
-			printf("ANGLE: %7.3f          \n", pos.angle);
-			printf("(deg): %7.3f          \n", pos.angle * 57.2957795f);
+			printf("FRAME: %u        \n", clock.frame_count);
+			printf("   dt: %7.3f ms  \n", clock.real_frame_time * 1000.0);
+			printf("POS X: %7.3f     \n", pos.x);
+			printf("POS Y: %7.3f     \n", pos.y);
+			printf("POS Z: %7.3f     \n", pos.z);
+			printf("ANGLE: %7.3f     \n", pos.angle);
+			printf("(deg): %7.3f     \n", pos.angle * 57.2957795f);
+			printf(" fell: %s        \n", dropped_to_death ? "yes" : "no");
 
-			if (clock.frame_count == 200)
+			if (clock.frame_count == 8)
 			{
-				const ds_pos warp_pos(82.6f, 22.5f, 107.4f, 1.56f);
+				dropped_to_death = false;
+			}
+
+			if (clock.frame_count == 24)
+			{
+				const ds_pos warp_pos(175.0f, 200.0f, 250.0f, 0.0f);
 				player.set_pos(warp_pos);
 			}
 
-			if (clock.frame_count > 400)
+			if (clock.frame_count > 100)
 			{
-				if (clock.frame_count < 1000)
+				if (clock.frame_count < 600)
 				{
-					const float forward_input = min(1.0f, static_cast<float>(clock.frame_count - 400) / 200.0f);
+					const float forward_input = min(1.0f, static_cast<float>(clock.frame_count - 100) / 200.0f);
 					state.set_left_stick(0.0f, forward_input);
 					device.update(state);
 					printf("input: forward %5.3f  \n", forward_input);
 				}
-				else
+				else if (clock.frame_count < 800)
 				{
 					state.reset();
 					device.update(state);
 					printf("input: OFF                \n");
-
-					if (clock.frame_count > 1200)
-					{
-						clock.reset();
-					}
 				}
+				else if (clock.frame_count >= 800 && !dropped_to_death)
+				{
+					const ds_pos warp_pos(182.0f, 250.0f, 250.0f, 0.0f);
+					player.set_pos(warp_pos);
+					dropped_to_death = true;
+				}
+			}
+
+			if (clock.frame_count > 1600)
+			{
+				clock.reset();
+				addresses.resolve(process);
 			}
 		}
 	}
