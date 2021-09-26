bd25acc2cb9bfc8b3f86970bfb434bbf6e5551d4
Alex Forsythe <awforsythe@gmail.com>
2021-09-25 21:37:04 -0500

    Replace yaml-cpp with rapidyaml

--- include/si_script.h
+++ include/si_script.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <string>
+
 struct si_script
 {
+	std::string name;
+
 	si_script();
 	bool load(const char* filepath);
 };
--- src/si_script.cpp
+++ src/si_script.cpp
@@ -1,9 +1,10 @@
 #include "si_script.h"
 
-#include <cassert>
-#include <string>
+#include <fstream>
+#include <vector>
 
-#include "yaml-cpp/yaml.h"
+#include "ryml_std.hpp"
+#include "ryml.hpp"
 
 si_script::si_script()
 {
@@ -11,23 +12,26 @@ si_script::si_script()
 
 bool si_script::load(const char* filepath)
 {
-	YAML::Node root;
-	try
-	{
-		root = YAML::LoadFile(filepath);
-		assert(root.IsDefined());
-	}
-	catch (YAML::BadFile&)
+	std::ifstream file;
+	file.open(filepath);
+	file.seekg(0, std::ios::end);
+	const size_t size = file.tellg();
+	file.seekg(0, std::ios::beg);
+
+	std::vector<char> buf(size);
+	file.read(buf.data(), size);
+	file.close();
+
+	ryml::substr view = c4::basic_substring<char>(buf.data(), size);
+	ryml::Tree tree = ryml::parse(view);
+	ryml::NodeRef root = tree.rootref();
+
+	if (!root.is_map() || !root.has_child("name"))
 	{
 		return false;
 	}
 
-	const YAML::Node name = root["name"];
-	if (!name.IsDefined())
-	{
-		return false;
-	}
-
-	printf("'%s': 'name' is '%s'\n", filepath, name.as<std::string>().c_str());
+	root["name"] >> name;
+	printf("'%s': 'name' is '%s'\n", filepath, name.c_str());
 	return true;
 }
